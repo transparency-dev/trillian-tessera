cbc54e9bccdbda9a8617440637b541d857bf575c
Roger Ng <rogerng@google.com>
2024-07-08 10:45:08 +0100

    Add a bare-bones outline of MySQL-based example personality and storage (#36)
    * Add a bare-bones outline of MySQL-based example personality and storage
    
    * Return `err` instead of `Exit` in mysql package

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	cloud.google.com/go/auth/oauth2adapt v0.2.2 // indirect
 	cloud.google.com/go/compute/metadata v0.3.0 // indirect
 	cloud.google.com/go/iam v1.1.8 // indirect
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/GoogleCloudPlatform/grpc-gcp-go/grpcgcp v1.5.0 // indirect
 	github.com/census-instrumentation/opencensus-proto v0.4.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
@@ -26,6 +27,7 @@ require (
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-sql-driver/mysql v1.8.1
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/s2a-go v0.1.7 // indirect
---
+++ storage/mysql/mysql.go
@@ -0,0 +1,41 @@
+// Copyright 2024 The Tessera authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package mysql contains a MySQL-based storage implementation for Tessera.
+package mysql
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+	"k8s.io/klog/v2"
+)
+
+// Storage is a MySQL-based storage implementation for Tessera.
+type Storage struct {
+	db *sql.DB
+}
+
+// New creates a new instance of the MySQL-based Storage.
+func New(db *sql.DB) (*Storage, error) {
+	s := &Storage{
+		db: db,
+	}
+	if err := s.db.Ping(); err != nil {
+		klog.Errorf("Failed to ping database: %v", err)
+		return nil, err
+	}
+
+	return s, nil
+}
