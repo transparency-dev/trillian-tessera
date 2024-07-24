23faa734e8cfe9a17b45a263baf9dd82e0561389
Martin Hutchinson <mhutchinson@gmail.com>
2024-07-24 14:06:26 +0100

    Fix origin flag removed in #81

--- testdata/build_log.sh
+++ testdata/build_log.sh
@@ -9,18 +9,17 @@ DIR=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 export LOG=${DIR}/log/
 export LOG_PRIVATE_KEY="PRIVATE+KEY+example.com/log/testdata+33d7b496+AeymY/SZAX0jZcJ8enZ5FY1Dz+wTML2yWSkK+9DSF3eg"
 export LOG_PUBLIC_KEY="example.com/log/testdata+33d7b496+AeHTu4Q3hEIMHNqc6fASMsq3rKNx280NI+oO5xCFkkSx"
-export ORIGIN="example.com/log/testdata"
 
 cd ${DIR}
 rm -fr log
 
-go run ../cmd/example-posix --storage_dir=${LOG} --initialise --origin="${ORIGIN}"
+go run ../cmd/example-posix --storage_dir=${LOG} --initialise
 cp ${LOG}/checkpoint ${LOG}/checkpoint.0
 
 export LEAF=`mktemp`
 for i in one two three four five six seven eit nain ten ileven twelf threeten fourten fivten; do
   echo -n "$i" > ${LEAF}
-  go run ../cmd/example-posix --storage_dir=${LOG} --origin="${ORIGIN}" --entries="${LEAF}"
+  go run ../cmd/example-posix --storage_dir=${LOG} --entries="${LEAF}"
   size=$(sed -n '2 p' ${LOG}/checkpoint)
   cp ${LOG}/checkpoint ${LOG}/checkpoint.${size}
 done
