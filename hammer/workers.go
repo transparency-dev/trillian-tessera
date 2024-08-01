1bf6daedcff236fd0417ece245a22c6bb66ef358
Al Cutter <al@google.com>
2024-08-01 16:52:59 +0100

    Don't complete bury pushback errs

--- hammer/workers.go
+++ hammer/workers.go
@@ -201,9 +201,7 @@ func (w *LogWriter) Run(ctx context.Context) {
 		lt := leafTime{queuedAt: time.Now()}
 		index, err := w.writer(ctx, newLeaf)
 		if err != nil {
-			if !errors.Is(ErrRetry, err) {
-				w.errChan <- fmt.Errorf("failed to create request: %w", err)
-			}
+			w.errChan <- fmt.Errorf("failed to create request: %w", err)
 			continue
 		}
 		lt.idx, lt.assignedAt = index, time.Now()
