4a6d2c3ef39f9fa9a8a4db45fff7dd101cebf176
Roger Ng <rogerng@google.com>
2024-08-01 16:00:49 +0100

    Force MySQL Go test to fail on GitHub workflow if the MySQL test database is not available (#107)

--- storage/mysql/mysql_test.go
+++ storage/mysql/mysql_test.go
@@ -10,7 +10,10 @@ import (
 	"k8s.io/klog/v2"
 )
 
-var mysqlURI = flag.String("mysql_uri", "root:root@tcp(localhost:3306)/test_tessera", "Connection string for a MySQL database")
+var (
+	mysqlURI            = flag.String("mysql_uri", "root:root@tcp(localhost:3306)/test_tessera", "Connection string for a MySQL database")
+	isMySQLTestOptional = flag.Bool("is_mysql_test_optional", true, "Boolean value to control whether the MySQL test is optional")
+)
 
 func TestMain(m *testing.M) {
 	klog.InitFlags(nil)
@@ -19,12 +22,18 @@ func TestMain(m *testing.M) {
 
 	db, err := sql.Open("mysql", *mysqlURI)
 	if err != nil {
-		klog.Errorf("MySQL not available, skipping all MySQL storage tests")
-		return
+		if *isMySQLTestOptional {
+			klog.Warning("MySQL not available, skipping all MySQL storage tests")
+			return
+		}
+		klog.Fatalf("Failed to open MySQL test db: %v", err)
 	}
 	if err := db.PingContext(ctx); err != nil {
-		klog.Errorf("MySQL not available, skipping all MySQL storage tests")
-		return
+		if *isMySQLTestOptional {
+			klog.Warning("MySQL not available, skipping all MySQL storage tests")
+			return
+		}
+		klog.Fatalf("Failed to ping MySQL test db: %v", err)
 	}
 	klog.Info("Successfully connected to MySQL test database")
 
