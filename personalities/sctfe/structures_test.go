d8d2342d7c74a25a83e49c899f11690893f5f54e
Philippe Boneff <phb@google.com>
2024-08-07 10:40:00 +0000

    drop dependency on c-t-go/testdata

--- personalities/sctfe/structures_test.go
+++ personalities/sctfe/structures_test.go
@@ -21,7 +21,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/google/certificate-transparency-go/trillian/testdata"
+	"github.com/transparency-dev/trillian-tessera/personalities/sctfe/testdata"
 )
 
 var (
