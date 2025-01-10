62515dbf309fb9f038f7174241ef930dcef92ed4
Al Cutter <al.cutter@gmail.com>
2025-01-10 15:54:52 +0000

    Add outline migration lib

---
+++ cmd/experimental/migrate/internal/migrate.go
@@ -0,0 +1,181 @@
+// Copyright 2024 The Tessera authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"context"
+	"fmt"
+	"sync/atomic"
+	"time"
+
+	"github.com/avast/retry-go/v4"
+	"github.com/transparency-dev/trillian-tessera/api/layout"
+	"github.com/transparency-dev/trillian-tessera/client"
+	"golang.org/x/sync/errgroup"
+	"k8s.io/klog/v2"
+)
+
+// copier controls the migration work.
+type copier struct {
+	// storage is the target we're migrating to.
+	storage    MigrationStorage
+	getEntries client.EntryBundleFetcherFunc
+
+	// sourceSize is the size of the source log.
+	sourceSize uint64
+	// sourceRoot is the root hash of the source log at sourceSize.
+	sourceRoot []byte
+
+	// todo contains work items to be completed.
+	todo chan bundle
+
+	// bundlesToMigrate is the total number of entry bundles which need to be copied.
+	bundlesToMigrate uint64
+	// bundlesMigrated is the number of entry bundles migrated so far.
+	bundlesMigrated atomic.Uint64
+}
+
+// bundle represents the address of an individual entry bundle.
+type bundle struct {
+	Index   uint64
+	Partial uint8
+}
+
+// MigrationStorage describes the required functionality from the target storage driver.
+type MigrationStorage interface {
+	// SetEntryBundle is called to store the provided entry bundle bytes at the given coordinates.
+	SetEntryBundle(ctx context.Context, index uint64, partial uint8, bundle []byte) error
+	// AwaitIntegration should block until the storage driver has received and integrated all outstanding entry bundles implied by sourceSize.
+	// An error should be returned if there is a problem integrating, or if the root hash of the local tree at size sourceSize does not match sourceRoot.
+	AwaitIntegration(ctx context.Context, sourceSize uint64, sourceRoot []byte) error
+	// Size returns the current integrated size of the local tree.
+	Size(ctx context.Context) (uint64, error)
+}
+
+// Migrate starts the work of copying sourceSize entries from the source to the target log.
+//
+// Only the entry bundles are copied as the target storage is expected to integrate them and recalculate the root.
+// This is done to ensure the correctness of both the source log as well as the copy process itself.
+//
+// A call to this function will block until either the copying is done, or an error has occurred.
+// It is an error if the resource copying completes ok but the resulting root hash does not match the provided sourceRoot.
+func Migrate(ctx context.Context, numWorkers int, sourceSize uint64, sourceRoot []byte, getEntries client.EntryBundleFetcherFunc, storage MigrationStorage) error {
+	klog.Infof("Starting migration; source size %d root %x", sourceSize, sourceRoot)
+
+	// TODO store state & resume
+	m := &copier{
+		storage:    storage,
+		sourceSize: sourceSize,
+		sourceRoot: sourceRoot,
+		getEntries: getEntries,
+		todo:       make(chan bundle, numWorkers*2),
+	}
+
+	// init
+	targetSize, err := m.storage.Size(ctx)
+	if err != nil {
+		return fmt.Errorf("Size: %v", err)
+	}
+	if targetSize > sourceSize {
+		return fmt.Errorf("Target size %d > source size %d", targetSize, sourceSize)
+	}
+	if targetSize == sourceSize {
+		return nil
+	}
+	go m.populateWork(targetSize, sourceSize)
+
+	// Print stats
+	go func() {
+		for {
+			time.Sleep(time.Second)
+			bn := m.bundlesMigrated.Load()
+			bnp := float64(bn*100) / float64(m.bundlesToMigrate)
+			s, err := m.storage.Size(ctx)
+			if err != nil {
+				klog.Warningf("Size: %v", err)
+			}
+			intp := float64(s*100) / float64(sourceSize)
+			klog.Infof("integration: %d (%.2f%%)  bundles: %d (%.2f%%)", s, intp, bn, bnp)
+		}
+	}()
+
+	// Do the copying
+	eg := errgroup.Group{}
+	for i := 0; i < numWorkers; i++ {
+		eg.Go(func() error {
+			return m.migrateWorker(ctx)
+		})
+	}
+	if err := eg.Wait(); err != nil {
+		return fmt.Errorf("migrate failed to copy resources: %v", err)
+	}
+
+	if err := m.storage.AwaitIntegration(ctx, sourceSize, sourceRoot); err != nil {
+		klog.Exitf("Migration failed: %v", err)
+	}
+	klog.Infof("Migration successful.")
+	return nil
+}
+
+// populateWork sends entries to the `todo` work channel.
+// Each entry corresponds to an individual entryBundle which needs to be copied.
+func (m *copier) populateWork(from, treeSize uint64) {
+	klog.Infof("Spans for entry range [%d, %d)", from, treeSize)
+	defer close(m.todo)
+	defer klog.Infof("total bundles to fetch %d", m.bundlesToMigrate)
+
+	r, err := layout.Range(from, treeSize-from, treeSize)
+	if err != nil {
+		klog.Exitf("Range(%d, %d): %v", from, treeSize-from, err)
+	}
+
+	for idx := r.StartIndex; idx <= r.EndIndex; idx++ {
+		var p uint8
+		switch idx {
+		case r.StartIndex:
+			p = r.StartPartial
+		case r.EndIndex:
+			p = r.EndPartial
+		}
+		m.todo <- bundle{Index: idx, Partial: p}
+		m.bundlesToMigrate++
+	}
+}
+
+// migrateWorker undertakes work items from the `todo` channel.
+//
+// It will attempt to retry failed operations several times before giving up, this should help
+// deal with any transient errors which may occur.
+func (m *copier) migrateWorker(ctx context.Context) error {
+	for b := range m.todo {
+		err := retry.Do(func() error {
+			d, err := m.getEntries(ctx, b.Index, uint8(b.Partial))
+			if err != nil {
+				return fmt.Errorf("failed to fetch entrybundle %d (p=%d): %v", b.Index, b.Partial, err)
+			}
+			if err := m.storage.SetEntryBundle(ctx, b.Index, b.Partial, d); err != nil {
+				return fmt.Errorf("failed to store entrybundle %d (p=%d): %v", b.Index, b.Partial, err)
+			}
+			m.bundlesMigrated.Add(1)
+			return nil
+		},
+			retry.Attempts(10),
+			retry.DelayType(retry.BackOffDelay))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	cloud.google.com/go/spanner v1.73.0
 	cloud.google.com/go/storage v1.50.0
 	github.com/RobinUS2/golang-moving-average v1.0.0
+	github.com/avast/retry-go/v4 v4.6.0
 	github.com/aws/aws-sdk-go-v2 v1.32.8
 	github.com/aws/aws-sdk-go-v2/config v1.28.10
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.51
