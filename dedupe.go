ad1067ff1740df355ba1652f11244fb54e32f355
Al Cutter <al@google.com>
2025-06-05 17:51:49 +0100

    Add some more tracing (#682)

--- dedupe.go
+++ dedupe.go
@@ -57,6 +57,9 @@ type inMemoryDedupe struct {
 // Add adds the entry to the underlying delegate only if e hasn't been recently seen. In either case,
 // an IndexFuture will be returned that the client can use to get the sequence number of this entry.
 func (d *inMemoryDedupe) add(ctx context.Context, e *Entry) IndexFuture {
+	ctx, span := tracer.Start(ctx, "tessera.Appender.inmemoryDedup.Add")
+	defer span.End()
+
 	id := string(e.Identity())
 
 	f := sync.OnceValue(func() IndexFuture {
